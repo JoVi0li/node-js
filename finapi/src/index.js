66d2e1651a9d582bbac9b36ac1e74580c14d7c34
JoViOli <jovi.oli04@gmail.com>
2022-03-16 10:43:16 -0300

    Projeto finalizado

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,5 +1,4 @@
-const { response } = require("express");
-const { application } = require("express");
+
 const express = require("express");
 const { v4: uuidv4 } = require("uuid")
 
@@ -9,13 +8,16 @@ app.use(express.json());
 
 const customers = [];
 
+
+///Middlewares
+
 function verifyIfExistsAccountByCPF(req, resp, next) {
   const { cpf } = req.params;
   const customer = customers.find((customer) => customer.cpf === cpf);
 
   if (!customer) {
     return resp.status(400).json({
-      error: "Customer Not Found"
+      messsage: "Usuário não encontrado"
     });
   }
 
@@ -23,6 +25,22 @@ function verifyIfExistsAccountByCPF(req, resp, next) {
 
   return next();
 }
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount
+    }
+
+  }, 0);
+
+  return balance;
+}
+
+
+///Endpoints
 
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
@@ -33,7 +51,7 @@ app.post("/account", (req, res) => {
 
   if (customerAlreadyExists) {
     return res.status(400).json({
-      error: "Customer Already Exists"
+      error: "Usuário já existe."
     });
   }
 
@@ -55,7 +73,7 @@ app.get("/statement/:cpf", verifyIfExistsAccountByCPF, (req, resp) => {
   });
 });
 
-app.post("/deposit/:cpf",verifyIfExistsAccountByCPF, (req, resp) => {
+app.post("/deposit/:cpf", verifyIfExistsAccountByCPF, (req, resp) => {
   const { description, amount } = req.body;
 
   const { customer } = req;
@@ -70,10 +88,95 @@ app.post("/deposit/:cpf",verifyIfExistsAccountByCPF, (req, resp) => {
   customer.statement.push(statementOperation);
 
   return resp.status(201).json({
-    message: "Transação realizada",
+    message: "Transação realizada.",
     statement: customer.statement
   })
 
+});
+
+app.post("/withdraw/:cpf", verifyIfExistsAccountByCPF, (req, resp) => {
+  const { amount } = req.body;
+
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return resp.status(400).json({
+      error: "Saldo insuficiente."
+    });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return resp.status(201).json({
+    message: "Saque realizado com sucesso.",
+    statement: customer.statement
+  });
+});
+
+app.get("/statement/:cpf/date", verifyIfExistsAccountByCPF, (req, resp) => {
+  const { customer } = req;
+  const { date } = req.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return resp.status(200).json({ statement });
+});
+
+app.put("/account/:cpf", verifyIfExistsAccountByCPF, (req, resp) => {
+  const { name } = req.body;
+  const { customer } = req;
+
+  customer.name = name;
+
+  return resp.status(201).json({
+    message: "Usuário atualizado com sucesso.",
+    customer
+  });
+});
+
+app.get("/account/:cpf", verifyIfExistsAccountByCPF, (req, resp) => {
+  const customer = customers.find(c => c.cpf === req.params.cpf);
+
+  return resp.status(200).json({
+    message: "Usuário encontrado.",
+    customer
+  });
+});
+
+app.delete("/account/:cpf", verifyIfExistsAccountByCPF, (req, resp) => {
+  const { customer } = req;
+
+  customers.splice(customer, 1);
+
+  return resp.status(200).json({
+    message: "Usuário excluido com sucesso.",
+    customers
+  });
+});
+
+app.get("/balance/:cpf", verifyIfExistsAccountByCPF, (req, resp) => {
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  return resp.status(200).json({
+    message: "Saldo recuperado com sucesso",
+    balance
+  });
 });
 
 app.listen(3333);
